0548ede587d734a8b36fbc8cc47d1f9a854f9166
liam s <liam.jacobsalangsang@gmail.com>
2023-12-22 17:54:21 +0700

    Added BreadCrumbs

---
+++ src/components/BreadCrumbs.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import { useNavigate } from 'react-router-dom';
+import { productsContext } from './context';
+
+
+export default function BreadCrumbs({thisItem}) {
+  const {categories} = React.useContext(productsContext)
+  const thisCategoryForItem = categories.find(category=> category.id == thisItem.category_id)
+  const navigate = useNavigate()
+
+  function handleClick(event) {
+    event.preventDefault();
+    console.log(event.target.name)
+    navigate(event.target.name)
+  }
+
+  return (
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs separator="›" aria-label="breadcrumb">
+      <Link underline="hover" name = "/products" color="inherit" >
+          All Products
+        </Link>
+        <Link underline="hover" name = "/discover" color="inherit" >
+         Categories
+        </Link>
+        <Link underline="hover" name = {`/${thisCategoryForItem.id}`} color="inherit" >
+          {thisCategoryForItem.name}
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          className='cursor-pointer'
+          name = {`/${thisItem.id}`
+          }
+        >
+          {thisItem.name}
+        </Link>
+      </Breadcrumbs>
+    </div>
+  );
+}
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,6 +4,7 @@ import { productsContext } from "../components/context";
 import Header from "../components/commonUI/Header";
 import { Rating } from "@mui/material";
 import toast from "react-hot-toast";
+import BreadCrumbs from '../components/BreadCrumbs'
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -27,42 +28,48 @@ const ProductDetails = () => {
   return (
     <>
       <Header />
+      
       {thisProduct ? (
-        <main className="h-screen  md:flex-row flex flex-col md:gap-[12rem] justify-center items-center ">
-          <div className=" md:min-w-[25rem] md:max-w-[30rem]  min-w-[15rem] max-w-[20rem]">
-            <img className="object-cover " src={thisProduct.image} alt="" />
-          </div>
-          <div>
-            <div>{thisProduct.name}</div>
-            {"deals" in thisProduct && thisProduct.deals[0] ? (
-              <div>
-                ${thisProduct.deals} discounted from ${thisProduct.price}{" "}
-              </div>
-            ) : (
-              <div>${thisProduct.price}</div>
-            )}
-            <div className="flex items-center">
-              <div className="mr-2">{thisProduct.rating}/5.0</div>
-              <Rating
-                name="read-only"
-                value={thisProduct.rating}
-                precision={0.2}
-                readOnly
-              />
-              <div className="ml-4 text-purple-500">
-                {thisProduct.reviews.length} Ratings
-              </div>
-            </div>
-            <div>{thisProduct.description}</div>
-            <button
-              onClick={addToCart}
-              className="mt-4 flex items-center gap-4 group/shoplink border-2 border-black p-2  font-bold hover:scale-110 hover:bg-purple-500 duration-200 ease-in"
-            >
-              {" "}
-              Add to Cart{" "}
-            </button>
-          </div>
-        </main>
+       <section>
+       
+         
+         <main className="h-screen  md:flex-row flex flex-col md:gap-[12rem] justify-center items-center ">
+           <div className=" md:min-w-[25rem] md:max-w-[30rem]  min-w-[15rem] max-w-[20rem]">
+            <BreadCrumbs thisItem = {thisProduct}/>
+             <img className="object-cover " src={thisProduct.image} alt="" />
+           </div>
+           <div>
+             <div>{thisProduct.name}</div>
+             {"deals" in thisProduct && thisProduct.deals[0] ? (
+               <div>
+                 ${thisProduct.deals} discounted from ${thisProduct.price}{" "}
+               </div>
+             ) : (
+               <div>${thisProduct.price}</div>
+             )}
+             <div className="flex items-center">
+               <div className="mr-2">{thisProduct.rating}/5.0</div>
+               <Rating
+                 name="read-only"
+                 value={thisProduct.rating}
+                 precision={0.2}
+                 readOnly
+               />
+               <div className="ml-4 text-purple-500">
+                 {thisProduct.reviews.length} Ratings
+               </div>
+             </div>
+             <div>{thisProduct.description}</div>
+             <button
+               onClick={addToCart}
+               className="mt-4 flex items-center gap-4 group/shoplink border-2 border-black p-2  font-bold hover:scale-110 hover:bg-purple-500 duration-200 ease-in"
+             >
+               {" "}
+               Add to Cart{" "}
+             </button>
+           </div>
+         </main>
+       </section>
       ) : null}
     </>
   );
